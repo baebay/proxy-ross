00b6eb30559eaa913a4a6603b4d7de312aafb5c8
Ross Calimlim <ross.calimlim@gmail.com>
2019-08-19 18:28:16 -0500

    Fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
   deploy: {
     production: {
       user: 'ross',
-      host: '134.209.69.8:~/',
+      host: '134.209.69.8',
       key: '~/.ssh/rosscalimlim.me',
       ref: 'origin/master',
-      repo: 'git@github.com:baebay/proxy-ross.git',
-      path: '/home/ubuntu/server/proxy',
+      repo: 'git@github.com:seabay-hratx42-fec/proxy-ross.git',
+      path: '/home/ross/server/proxy',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js',
     },
   },
