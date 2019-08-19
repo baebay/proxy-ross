80e27bcdcb09265de7319436294bd266ef6c6362
Ross Calimlim <ross.calimlim@gmail.com>
2019-08-19 18:12:44 -0500

    Serve react scripts properly

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.SERVER_PORT;
 app.use(cors());
 app.use(compression());
 app.use('/projects/seabay/app', express.static('public'));
-app.use('/scripts', express.static('scripts'));
+app.use('/projects/seabay/app/scripts', express.static('scripts'));
 app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on ${port}`));
